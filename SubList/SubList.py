a4d9fad676e74db2b9201347e139013b25a9db94
Yawnmain <108503319+Yawnmain@users.noreply.github.com>
2022-11-21 19:07:55 +0800

    Create SubList.py

---
+++ SubList/SubList.py
@@ -0,0 +1,12 @@
+class SubList(list):
+    def __init__(self, *lst):
+        super(SubList, self).__init__(*lst)
+
+    def __sub__(self, other):
+        return self.__class__([e for e in self if e not in other])
+
+
+a = SubList([1, 2, 3, 4, 5, 7])
+b = SubList([1, 2, 5])
+c = a - b
+print(c)
